a21f736fb2e514a4129587cfc38453f39cfb6858
Alexandre Roba <alexandre.roba@arobanet.be>
2015-10-12 14:07:12 +0000

    Added the first working angular application

---
+++ public/app/app.ts
@@ -0,0 +1,7 @@
+/// <reference path='../../typings/angularjs/angular.d.ts'/>
+
+angular.module("app",[]);
+
+angular.module("app").controller("testCtrl",function($scope){
+   $scope.test = "working!"; 
+});
