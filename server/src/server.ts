301c546bdf837c14d047101f67c4a2c890200cb1
Alexandre Roba <alexandre.roba@arobanet.be>
2015-10-14 12:32:26 +0000

    removed bower for client side library and replacing them by their node modules sibling

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,7 +8,7 @@ app.set("views", __dirname + "/../views");
 app.set("view engine", "jade");
 
 app.use(express.static(__dirname + "/../../public"));
-app.use("/vendor",  express.static(__dirname + "/../../bower_components"));
+app.use("/vendor",  express.static(__dirname + "/../../node_modules"));
 
 app.get("*", function(req, res){
     res.render("index");
