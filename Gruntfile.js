69e90820213983c50d115b328c74fa35e2854213
Alexandre Roba <alexandre.roba@arobanet.be>
2015-10-18 09:52:39 +0000

    working on the deployment pipeline

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,14 +4,18 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-mocha-test');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-tslint');
+    grunt.loadNpmTasks('grunt-github-release-asset');
+    grunt.loadNpmTasks('grunt-git-remote-tag');
+    grunt.loadNpmTasks('grunt-version-bump');
+
     //Project Configuration
     grunt.initConfig({
         ts: {
             server: {
                 src: [
                     'server/src/**/*.ts'
-                    ],
-                dest:'server/build',
+                ],
+                dest: 'server/build',
                 options: {
                     fast: 'never',
                     module: 'commonjs',
@@ -21,11 +25,11 @@ module.exports = function(grunt) {
                     declaration: false
                 }
             },
-            client:{
+            client: {
                 src: [
                     'public/app/src/**/*.ts'
-                    ],
-                dest:'public/app/build',
+                ],
+                dest: 'public/app/build',
                 options: {
                     fast: 'never',
                     module: 'commonjs',
@@ -48,13 +52,15 @@ module.exports = function(grunt) {
         },
         clean: {
             buildOutput: [
-                'server/build', 
+                'server/build',
                 'public/app/build',
-                'test/build', 
-                'spikes/build'],
-            baseDir:[
+                'test/build',
+                'spikes/build'
+            ],
+            baseDir: [
                 'server/**/.baseDir.*',
-                'public/**/.baseDir.*']
+                'public/**/.baseDir.*'
+            ]
         },
         tslint: {
             options: {
@@ -64,13 +70,34 @@ module.exports = function(grunt) {
                 src: [
                     'server/src**/*.ts',
                     'public/app/src/**/*.ts',
-                    'test/src/**/*.ts', 
-                    'spikes/src/**/*.ts']
+                    'test/src/**/*.ts',
+                    'spikes/src/**/*.ts'
+                ]
+            }
+        },
+        githubAsset: {
+            options: {
+                credentials: grunt.file.readJSON('githubtoken.json'),
+                repo: 'git@github.com:AlexandreRoba/jobfinder.git',
+                file: 'JobFinder-0.0.1.tgz',
+                releaseName: 'Version {tag}'
+            }
+        },
+        gitRemoteTag: {
+            release: {
+                options: {
+                    tag: util.format('v.%s-%s', process.env.BUILD_NUMBER, process.env.ENVIRONMENT),
+                    remote: 'origin',
+                    //message: 'It worked!',
+                    force: false
+                },
+                src: './'
             }
         }
     });
+
     //Define the default task
-    grunt.registerTask('default', ['clean:buildOutput', 'tslint', 'ts:server', 'ts:client','clean:baseDir']);
+    grunt.registerTask('default', ['clean:buildOutput', 'tslint', 'ts:server', 'ts:client', 'clean:baseDir']);
     grunt.registerTask('test', ['default', 'mochaTest']);
-    grunt.registerTask('deploy',['clean']);
+    grunt.registerTask('deploy', ['githubAsset']);
 };
