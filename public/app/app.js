1c289fc41a3cc4959a5704c8fd048028fa33bc73
Alexandre Roba <alexandre.roba@arobanet.be>
2015-10-30 20:58:26 +0100

    Connected the client application to the jobs api

--- public/app/app.js
+++ public/app/app.js
@@ -1,7 +1,6 @@
 /// <reference path='../../typings/angularjs/angular.d.ts'/>
-angular.module("app", []);
-angular.module("app").controller("testCtrl", function ($scope) {
-    $scope.jobs = [
-        { title: "Sales Person", description: "You will fight dragons" },
-        { title: "Developer Person", description: "You will develop dragons" }];
+/// <reference path='../../typings/angularjs/angular-resource.d.ts'/>
+angular.module("app", ["ngResource"]);
+angular.module("app").controller("testCtrl", function ($scope, $resource) {
+    $scope.jobs = $resource("api/jobs").query();
 });
